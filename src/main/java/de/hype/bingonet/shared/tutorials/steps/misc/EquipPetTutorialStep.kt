e9a16c5d993f4dfceedc4e41a933f87ef5b0b24f
HacktheTime <l4bg0jb7@duck.com>
2025-08-17 21:32:25 +0200

    added pet check tutorial step

---
+++ src/main/java/de/hype/bingonet/shared/tutorials/steps/misc/EquipPetTutorialStep.kt
@@ -0,0 +1,11 @@
+package de.hype.bingonet.shared.tutorials.steps.misc
+
+import at.hannibal2.skyhanni.utils.LorenzRarity
+import at.hannibal2.skyhanni.utils.NeuInternalName
+
+class EquipPetTutorialStep(
+    val petType: NeuInternalName,
+    val minimumPetRarity: LorenzRarity
+) {
+    //TODO make it so highest pet level is used.
+}
