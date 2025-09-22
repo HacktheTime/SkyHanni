b97ab529c46bbfa4d11fa824729d108b76f817c9
HacktheTime <l4bg0jb7@duck.com>
2025-09-22 17:56:39 +0200

    NeuMinionTypeData.kt fix

--- src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/neu/NeuMinionTypeData.kt
+++ src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/neu/NeuMinionTypeData.kt
@@ -1,5 +1,6 @@
 package at.hannibal2.skyhanni.data.jsonobjects.repo.neu
 
+import at.hannibal2.skyhanni.utils.NeuInternalName
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import de.hype.bingonet.sharedcompilation.sbenums.BNNEUItem
@@ -21,9 +22,9 @@ class NeuMinionTypeData : MinionType {
 
     @Expose
     @SerializedName("drops")
-    override val drops: Map<BNNEUItem, Double>
+    override val drops: Map<NeuInternalName, Double>
 
-    private constructor(typeId: String, category: MinionCategory, requiredActions: Int, drops: Map<BNNEUItem, Double>) : super(
+    private constructor(typeId: String, category: MinionCategory, requiredActions: Int, drops: Map<NeuInternalName, Double>) : super(
         typeId, category, drops, requiredActions,
     ) {
         this.typeId = typeId
