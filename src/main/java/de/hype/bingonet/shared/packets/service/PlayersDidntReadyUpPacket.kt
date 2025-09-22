2852a9c68bb2c3a32c61bb7da530093d9450df24
HacktheTime <l4bg0jb7@duck.com>
2025-09-22 17:57:21 +0200

    PlayersDidntReadyUpPacket.kt fix

--- src/main/java/de/hype/bingonet/shared/packets/service/PlayersDidntReadyUpPacket.kt
+++ src/main/java/de/hype/bingonet/shared/packets/service/PlayersDidntReadyUpPacket.kt
@@ -1,9 +1,9 @@
 package de.hype.bingonet.shared.packets.service
 
 import de.hype.bingonet.environment.packetconfig.AbstractPacket
-import de.hype.bingonet.server.objects.BBUser
+import de.hype.bingonet.shared.objects.BNUser
 
 /**
  * send when the timeout for the ready up has been reached and not everyone readied up.
  */
-class PlayersDidntReadyUpPacket(var serverId: Int, var participiants: MutableSet<BBUser>) : AbstractPacket()
+class PlayersDidntReadyUpPacket(var serverId: Int, var participiants: MutableSet<BNUser>) : AbstractPacket()
