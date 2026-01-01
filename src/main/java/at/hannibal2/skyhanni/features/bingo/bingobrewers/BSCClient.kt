2eb1a7c54336dc4f2a6cb962aa02b9a808cce29d
HacktheTime <l4bg0jb7@duck.com>
2026-01-01 20:56:27 +0100

    fixed Bingo Splash Community missing a worker thread and added a command to reconnect.

--- src/main/java/at/hannibal2/skyhanni/features/bingo/bingobrewers/BSCClient.kt
+++ src/main/java/at/hannibal2/skyhanni/features/bingo/bingobrewers/BSCClient.kt
@@ -2,9 +2,9 @@ package at.hannibal2.skyhanni.features.bingo.bingobrewers
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.config.commands.CommandCategory
+import at.hannibal2.skyhanni.config.commands.CommandRegistrationEvent
 import at.hannibal2.skyhanni.events.ConfigLoadEvent
-import at.hannibal2.skyhanni.features.bingo.bingobrewers.BingoBrewersClient.isEnabled
-import at.hannibal2.skyhanni.features.bingo.bingobrewers.BingoBrewersClient.stop
 import at.hannibal2.skyhanni.features.bingo.bingonet.SplashManager
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
@@ -21,16 +21,21 @@ object BSCClient {
     val config = SkyHanniMod.feature.event.bingo.bingoNetworks
     val enabled get() = config.useBSC
     var client: Socket? = null
+    lateinit var thread: Thread
 
     @HandleEvent
     fun event(event: ConfigLoadEvent) {
         init()
     }
 
+    init {
+        init()
+    }
+
     @Synchronized
     fun init() {
-        if (isEnabled()) {
-            SkyHanniMod.launchCoroutine("Init BSC Client after ConfigLoadEvent") {
+        if (enabled) {
+            SkyHanniMod.launchCoroutine("Init BSC Client") {
                 if (client?.isConnected != true) connect()
             }
         } else {
@@ -45,50 +50,79 @@ object BSCClient {
         client?.close()
         val client = Socket("bsn.morazzer.dev", 1807)
         this.client = client
-        val inputStream = client.getInputStream()
-        val buffer = ByteArray(1024)
-        val read = inputStream.read(buffer)
-        if (read == -1) {
-            ChatUtils.clickableChat(
-                "Error trying to connect to BSC Server",
-                onClick = {
-                    connect()
-                },
-            )
-            BSCClient.client?.close()
+        thread = Thread {
+            while (!thread.isInterrupted && client.isConnected) {
+                val inputStream = client.getInputStream()
+                val buffer = ByteArray(1024)
+                val read = inputStream.read(buffer)
+                if (read == -1) {
+                    ChatUtils.clickableChat(
+                        "Error trying to connect to BSC Server",
+                        onClick = {
+                            connect()
+                        },
+                    )
+                    BSCClient.client?.close()
+                }
+                val id = buffer[0]
+                if (id == 0.toByte()) {
+                    //Ignore
+                } else if (id == 1.toByte()) {
+                    ChatUtils.chat("§aConnected to Bingo Splash Community Splash Announcement Server!")
+                    val message = String(buffer, 1, read - 1).let {
+                        it.replace("<@&[0-9]+>".toRegex(), "")
+                    }
+                    val serverId = Pattern.compile("((mini|mega)[0-9]+[a-z])", Pattern.CASE_INSENSITIVE).matcher(message).let {
+                        it.find()
+                        it.group(1)
+                    }
+                    val hubNumber = Pattern.compile("(hub ([0-9]{1,2}))", Pattern.CASE_INSENSITIVE).matcher(message).let {
+                        it.find()
+                        it.group(2)?.toIntOrNull()
+                    }
+                    val island = if (message.contains("dhub", true) || message.contains("dungeon")) Islands.DUNGEON_HUB else Islands.HUB
+                    val hubSelectorData = if (hubNumber != null) SplashData.HubSelectorData(hubNumber, island) else null
+                    if (hubSelectorData == null) continue
+                    val sploosh = SplashData(
+                        announcer = "BSC",
+                        locationInHub = SplashLocations.KAT,
+                        extraMessage = message,
+                        lessWaste = false,
+                        serverID = serverId,
+                        hubSelectorData = hubSelectorData,
+                        status = StatusConstants.WAITING,
+                        funder = null,
+                    )
+                    SplashManager.addSplash(sploosh, SplashManager.SplashSource.BSC)
+                } else if (id == 2.toByte()) {
+                    ChatUtils.chat("§cConnection limit on BSC server reached. Try again later with /bsc reconnect")
+                    BSCClient.client?.close()
+                    return@Thread
+                }
+            }
         }
-        val id = buffer[0]
-        if (id == 0.toByte()) {
-            //Ignore
-        } else if (id == 1.toByte()) {
-            ChatUtils.chat("§aConnected to Bingo Splash Community Splash Announcement Server!")
-            val message = String(buffer, 1, read - 1).let {
-                it.replace("<@&[0-9]+>".toRegex(),"")
-            }
-            val serverId = Pattern.compile("((mini|mega)[0-9]+[a-z])", Pattern.CASE_INSENSITIVE).matcher(message).let {
-                it.find()
-                it.group(1)
-            }
-            val hubNumber = Pattern.compile("(hub ([0-9]{1,2}))", Pattern.CASE_INSENSITIVE).matcher(message).let {
-                it.find()
-                it.group(2)?.toIntOrNull()
-            }
-            val island = if (message.contains("dhub", true) || message.contains("dungeon")) Islands.DUNGEON_HUB else Islands.HUB
-            val hubSelectorData = if (hubNumber != null) SplashData.HubSelectorData(hubNumber, island) else null
-            if (hubSelectorData == null) return
-            val sploosh = SplashData(
-                announcer = "BSC",
-                locationInHub = SplashLocations.KAT,
-                extraMessage = message,
-                lessWaste = false,
-                serverID = serverId,
-                hubSelectorData = hubSelectorData,
-                status = StatusConstants.WAITING,
-                funder = null,
-            )
-            SplashManager.addSplash(sploosh, SplashManager.SplashSource.BSC)
-        }
-
+        thread.start()
     }
 
+
+    @HandleEvent
+    fun onCommandRegistration(event: CommandRegistrationEvent) {
+        event.registerBrigadier("bsc") {
+            description = "Copies information about the item in hand to the clipboard"
+            category = CommandCategory.USERS_ACTIVE
+            literalCallback("reconnect"){
+                ChatUtils.chat("§eReconnecting to BSC Server...")
+                connect()
+            }
+            literalCallback("stop"){
+                ChatUtils.chat("§eDisconnecting from BSC Server...")
+                stop()
+            }
+        }
+    }
+
+    fun stop() {
+        client?.close()
+        thread.interrupt()
+    }
 }
