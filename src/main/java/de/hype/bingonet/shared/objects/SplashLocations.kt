a65a6b904043ea832084df48cb6f0ce5269069ad
HacktheTime <l4bg0jb7@duck.com>
2026-03-01 13:22:28 +0100

    upated bb splash location.

--- src/main/java/de/hype/bingonet/shared/objects/SplashLocations.kt
+++ src/main/java/de/hype/bingonet/shared/objects/SplashLocations.kt
@@ -2,7 +2,7 @@ package de.hype.bingonet.shared.objects
 
 object SplashLocations {
     val HUB_SELECTOR: SplashLocation = SplashLocation("Hub Selector Corner", -9, 70, -21)
-    val BB_MAP: SplashLocation = SplashLocation("Map", -6, 70, -6)
+    val BB_MAP: SplashLocation = SplashLocation("Map", 6, 69, -6)
 
 
     @JvmStatic
