2086241f31be85be0d8eb25d366fe131214454da
HacktheTime <l4bg0jb7@duck.com>
2026-03-02 10:06:06 +0100

    updated and added splash locations

--- src/main/java/de/hype/bingonet/shared/objects/SplashLocations.kt
+++ src/main/java/de/hype/bingonet/shared/objects/SplashLocations.kt
@@ -2,12 +2,19 @@ package de.hype.bingonet.shared.objects
 
 object SplashLocations {
     val HUB_SELECTOR: SplashLocation = SplashLocation("Hub Selector Corner", -9, 70, -21)
-    val BB_MAP: SplashLocation = SplashLocation("Map", 6, 69, -6)
+    val MAP_BB = SplashLocation("Hub Map(BB)", 6, 69, -6)
+    val KAT_BSC = SplashLocation("Kat (BSC)", 8, 72, -54)
+    val COMMUNITY_CENTER_CN = SplashLocation("Painting in Community Center (CN)", 0, 79, 21)
 
 
     @JvmStatic
     fun values(): List<SplashLocation> {
-        return listOf(HUB_SELECTOR)
+        return listOf(
+            HUB_SELECTOR,
+            MAP_BB,
+            KAT_BSC,
+            COMMUNITY_CENTER_CN,
+        )
     }
 
     @JvmStatic
