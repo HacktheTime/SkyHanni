c087d8348f44e8194039b24976036ab3deec4f51
HacktheTime <l4bg0jb7@duck.com>
2026-07-24 02:28:23 +0200

    fixed an issue with carrot candy helper

--- src/main/java/at/hannibal2/skyhanni/features/bingo/card/PetXPComGoalKeybind.kt
+++ src/main/java/at/hannibal2/skyhanni/features/bingo/card/PetXPComGoalKeybind.kt
@@ -2,23 +2,24 @@ package at.hannibal2.skyhanni.features.bingo.card
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
-import at.hannibal2.skyhanni.events.minecraft.KeyDownEvent
+import at.hannibal2.skyhanni.events.minecraft.KeyPressEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.HypixelCommands
 import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.getInternalName
 import at.hannibal2.skyhanni.utils.KeyboardManager.isKeyClicked
 import at.hannibal2.skyhanni.utils.SkyBlockItemModifierUtils.getItemId
-import org.lwjgl.glfw.GLFW
+import at.hannibal2.skyhanni.utils.compat.MinecraftCompat
 
 @SkyHanniModule
 object PetXPComGoalKeybind {
     val config get() = SkyHanniMod.feature.event.bingo
 
     @HandleEvent(onlyOnSkyblock = true)
-    fun onKeyDown(event: KeyDownEvent) {
+    fun onKeyClicked(event: KeyPressEvent) {
+        if (MinecraftCompat.screen != null) return
         val key = config.petXpComGoalKeybind
-        if (key == GLFW.GLFW_KEY_UNKNOWN) return
+        if (event.keyCode != key) return
         if (key.isKeyClicked()) {
             if (InventoryUtils.getItemInHand()?.getInternalName()?.internalName.equals("SIMPLE_CARROT_CANDY")) {
                 HypixelCommands.pet()
