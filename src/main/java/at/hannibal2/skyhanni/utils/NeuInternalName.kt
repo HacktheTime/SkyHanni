5e2ada6c5edcd6a38eca03fa17f9ebdd4a969220
HacktheTime <l4bg0jb7@duck.com>
2026-08-13 22:58:02 +0200

    fixed merge issue

--- src/main/java/at/hannibal2/skyhanni/utils/NeuInternalName.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/NeuInternalName.kt
@@ -72,8 +72,6 @@ open class NeuInternalName private constructor(internalName: String) : BNNEUItem
         val SKYBLOCK_GOLD_MEDAL = "SKYBLOCK_GOLD_MEDAL".toInternalName()
         val SKYBLOCK_SILVER_MEDAL = "SKYBLOCK_SILVER_MEDAL".toInternalName()
         val SKYBLOCK_BRONZE_MEDAL = "SKYBLOCK_BRONZE_MEDAL".toInternalName()
-        val SKYBLOCK_COPPER = "SKYBLOCK_COPPER".toInternalName()
-
         val WISP_POTION = "WISP_POTION".toInternalName()
         val ENCHANTED_HAY_BLOCK = "ENCHANTED_HAY_BLOCK".toInternalName()
         val TIGHTLY_TIED_HAY_BALE = "TIGHTLY_TIED_HAY_BALE".toInternalName()
